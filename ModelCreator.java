7ee321146efafdf63d1c406d444c5dc95d2e991e
KaleProgram <111293992+KaleProgram@users.noreply.github.com>
2022-11-07 11:09:15 -0600

    Create ModelCreator.java

---
+++ ModelCreator.java
@@ -0,0 +1,57 @@
+
+public static ModelCreator()
+{
+  private String UUID;
+  public ModelCreator()
+  {
+    UUID = getUUID;
+  
+  }
+  
+  
+  public CreateModel()
+  {
+    String first = 0;
+    for(int x = 0;x<UUID.length;x++)
+    {
+      if(UUID[x]=="a")
+      {
+        frist= frist +UUID[x]=11;
+      }
+      else if(UUID[x]=="b")
+      {
+        first = first+UUID[x]=12;
+      }
+      else if(UUID[x]=="c")
+      {
+        first = first + UUID[x]=13;
+      }
+      else if(UUID[x]=="d")
+      {
+        first = first+UUID[x]=14;
+      }
+      else if(UUID[x]=="e")
+      {
+        first = first+UUID[x]=15;
+      }
+      else if(UUID[x]=="f")
+      {
+        first= first+UUID[x]=16;
+      }
+      else
+      {
+        first = first + UUID[x];
+      }
+    }
+    int first = (String)first;
+  
+    
+    
+    
+    
+    
+  }
+
+
+
+}
