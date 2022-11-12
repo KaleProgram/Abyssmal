5c387ea7d99aed15f57c2a891d57445b04db0e28
KaleProgram <111293992+KaleProgram@users.noreply.github.com>
2022-11-12 14:44:56 -0600

    Create TransBlock.java

---
+++ src/main/java/com/kale/Abyssmal/TransBlock.java
@@ -0,0 +1,42 @@
+package com.kale.Abyssmal
+
+
+import net.minecraft.block.Block;
+import net.minecraft.block.material.Material;
+import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.util.BlockRenderLayer;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
+
+public TransBlock extends Block
+{
+  public static final String NAME = "Transmogification block";
+  private static final String UNLOCALIZED_NAME = "TransBlock";
+  private static final String REGISTRY_NAME = "TransBlock_registry";
+
+
+
+
+
+  public TransBlock()
+  {
+    Super(Material.ROCK);
+    
+    
+    this.setCreativeTab(CreativeTab.BUILDING_BLOCKS);
+    
+    this.setUnlocalizedName(UNLOCALIZED_NAME);
+    this.setRegistryName(REGISTRY_NAME);
+  }
+  
+  
+  @SideOnly (Side.Client)
+  public BlockRenderLayer getBlockLayer()
+  {
+    return BlockRenderLayer.SOLID;
+  }
+  
+
+
+
+}
